6e3d7917edb8d347d7b903e27b488ebdc6d17af7
zjd <780891419@qq.com>
2022-01-06 16:57:20 +0800

    dev2

--- src/test/P.java
+++ src/test/P.java
@@ -4,6 +4,8 @@ public class P {
     public static void main(String[] args) {
         System.out.println(11);
         System.out.println(1112);
+        System.out.println(11);
+        System.out.println(1112);
     }
 
 }
